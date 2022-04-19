59f275a503787380af14c80c342af7314d513769
bart <bart_klumperman@Live.nl>
2022-04-19 23:18:58 +0200

    implemented homepage random dishes and menu card

--- menu/src/Controller/HomeController.php
+++ menu/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GerechtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(GerechtRepository $gerechtenData): Response
     {
+
+        $gerechten = $gerechtenData->findAll();
+        $randomGerecht = array_rand($gerechten, 2);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'gerecht1' => $gerechten[$randomGerecht[0]],
+            'gerecht2' => $gerechten[$randomGerecht[1]],
         ]);
     }
 }
--- menu/src/Controller/MenuController.php
+++ menu/src/Controller/MenuController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\GerechtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MenuController extends AbstractController
 {
-    #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    #[Route('/menukaart', name: 'app_menu')]
+    public function menukaart(GerechtRepository $gerechtenData): Response
     {
+        $gerechten = $gerechtenData->findAll();
+
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            'gerechten' => $gerechten,
         ]);
     }
 }
